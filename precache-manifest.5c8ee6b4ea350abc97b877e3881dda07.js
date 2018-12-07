6f1047dd9cb64ac3c0901643f8ecbe1022f9b82e
sellsj14 <sellsj14@gmail.com>
2018-12-07 18:45:04 -0500

    Updates

---
+++ precache-manifest.5c8ee6b4ea350abc97b877e3881dda07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Pioneer18/my_portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2ce9d72d68d98b8410d9",
+    "url": "/Pioneer18/my_portfolio/static/js/runtime~main.2ce9d72d.js"
+  },
+  {
+    "revision": "9d5a1859445100b97366",
+    "url": "/Pioneer18/my_portfolio/static/js/main.9d5a1859.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/Pioneer18/my_portfolio/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "9d5a1859445100b97366",
+    "url": "/Pioneer18/my_portfolio/static/css/main.2172ac90.chunk.css"
+  },
+  {
+    "revision": "39fb242022f1f9fe9674e2bd5e41b975",
+    "url": "/Pioneer18/my_portfolio/index.html"
+  }
+];
