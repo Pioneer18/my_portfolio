5569cbfebd5495a30316664b32552daf2ec8e13a
sellsj14 <sellsj14@gmail.com>
2018-12-09 18:46:22 -0500

    got the navbar, working on jumbotron

--- jonathansells/src/components/Navigation/Navigation.js
+++ jonathansells/src/components/Navigation/Navigation.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import "./Navigation.css";
 import {
@@ -8,7 +9,11 @@ import {
   Nav,
   NavItem,
   NavLink,
-  } from 'reactstrap';
+  } from 'mdbreact';
+  import {
+    Jumbotron,
+    Container
+  } from 'reactstrap'
 
 export default class Navigation extends React.Component {
   constructor(props) {
@@ -25,17 +30,17 @@ export default class Navigation extends React.Component {
     });
   }
   render() {
+      const brand = {  color:"#F0DB4F"}
       const text = {color:"#fff", padding: "0 1rem"}
       const navBack = {
-          backgroundColor: "#000",
-          color: "#F0DB4F", 
+          backgroundColor: "#000", 
           padding: ".5% 10%",
         }
 
     return (
       <div>
-        <Navbar style={navBack}  light expand="md">
-            <NavbarBrand to="/">JS</NavbarBrand>
+        <Navbar light style={navBack}  expand="md">
+            <NavbarBrand style={brand} to="/">JS</NavbarBrand>
             <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
                 <Nav  className="ml-auto" navbar>
@@ -51,6 +56,12 @@ export default class Navigation extends React.Component {
                 </Nav>
             </Collapse>
         </Navbar>
+        <Jumbotron className="jumboImg" fluid>
+        <Container fluid>
+          <h1 className="display-3">Hello, I'm<span style={brand} className="jsells"> Jonathan Sells</span><br/>I'm a full-stack web &amp; IoT Developer</h1>
+          <p className="lead">button to be</p>
+        </Container>
+      </Jumbotron>
       </div>
     );
   }
--- jonathansells/src/index.js
+++ jonathansells/src/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {BrowserRouter as Router, Route } from "react-router-dom";
+
 //===========imported mdbreact style files===============
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
+import 'bootstrap/dist/css/bootstrap.css';
 //=======================================================
 import App from './App';
+import Navigation from "./components/Navigation";
 import * as serviceWorker from './serviceWorker';
 //import 'font-awesome/css/font-awesome.min.css'; 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister();*/
+
+
+ReactDOM.render(
+        <Router>     
+            <App>
+                <Route exact path="/"  component={Navigation} />
+            </App>    
+        </Router>,
+    document.getElementById("root")
+    );
