01428eede99779b2aaffdb5f14bb89127ef0e8b1
sellsj14 <sellsj14@gmail.com>
2018-12-10 14:56:02 -0500

    added the section-divider component

--- jonathansells/src/App.js
+++ jonathansells/src/App.js
@@ -3,6 +3,7 @@ import "./"
 import './App.css';
 import Navigation from "./components/Navigation";
 import Parallax from "./components/Parallax";
+import SectionDivider from "./components/SectionDivder";
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
        <React.Fragment>
          <Navigation />
          <Parallax />
+         <SectionDivider />
        </React.Fragment>
       </div>
     );
---
+++ jonathansells/src/components/SectionDivder/SectionDivider.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./SectionDivider.css";
+const SectionDivider = props => (
+    <div  className="section-divider">
+        <h3>About Me</h3>
+    </div>
+)
+
+export default SectionDivider;
