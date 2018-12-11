bbdf34de3f36f12b27600ee21b8817ba07689c69
sellsj14 <sellsj14@gmail.com>
2018-12-10 21:31:26 -0500

    timeline completed, now cleaning up minor css issues before moving on to gallery

--- jonathansells/src/components/Timeline/data.js
+++ jonathansells/src/components/Timeline/data.js
@@ -14,33 +14,39 @@ const data = {
     },
     panel2:{
         title: "TEACHING & STUDYING IN JAPAN",
-        text: "I was not always a tech person, In fact I daul majored in international politics & Japanese Language and Literature. However, by my senior year, I decided against pursuing a political career and began searching for other possible career paths. During my final semester of college I took an introduction to Python course and found that I am far more interested in technology than politics.",
-        date: "May-2014",
+        text: "After graduating from W&L I was still unsure of what to do with my recently sparked interest in programming, so I chose to teach English in Japan for a year. While teaching, exploring, and soaking up the experiences of Japan, I continued to work through the exercises in my intro to Python textbook and I also began to study HTML and CSS. I relied on resources like Udemy, Codecademy, and Khan Academy to dive deeper into programming fundamentals, and I also discovered a new interest in Arduinos by reading coding blogs and forums. I wasn’t sure that programming microcontrollers could lead to a career, but the low cost and easy enough interface of Arduinos enticed me to try coding with them anyways.",
+        date: "Nov-2014",
         src: source2
     },
     panel3:{
         title: "UBREAKIFIX, ARDUINOS, & MY PARENTS' GARAGE",
-        text: "I was not always a tech person, In fact I daul majored in international politics & Japanese Language and Literature. However, by my senior year, I decided against pursuing a political career and began searching for other possible career paths. During my final semester of college I took an introduction to Python course and found that I am far more interested in technology than politics.",
-        date: "May-2014",
+        text: "On November 10th, 2015 I made my final decision to not work another year in Japan, but to instead return home and help my brother with his Ubreakifix repair shops. The opportunity to work with my brother as well as learn the ins and outs of repairing electronics was too good to pass, it seemed like a nice bridge into a tech career. Eager to put my Python Coding skills to the test, I also began programming some hello world arduino circuits as well as designing a few projects of my own. Soon I was so invested in testing my new ideas that I used a portion of the money I saved working in Japan to wall off a third of my parents’ garage, which built a small workshop for projects.",
+        date: "Dec-2015",
         src: source6
     },
     panel4:{
         title: "ARDUINOS, & THE SLOW FOOD MOVEMENT",
-        text: "I was not always a tech person, In fact I daul majored in international politics & Japanese Language and Literature. However, by my senior year, I decided against pursuing a political career and began searching for other possible career paths. During my final semester of college I took an introduction to Python course and found that I am far more interested in technology than politics.",
-        date: "May-2014",
-        src: source4
+        text: "Once the workshop was created, I spent most of my free time thinking up interesting projects to create with my new favorite toy. Since college I have had an interest in environmental conservation and trends like the Slow Food Movement. So I combined the programming and hardware skills I had been studying to create automated indoor gardening systems. With time my projects ranged from simple Home Depot buckets with small pumps for watering plants, to a fully automated aquaponic system that filled the entire room, and eventually even a condensed refrigerator sized food computer equipped with a filter of microbes to organically fertilize the enclosed plants. Over the course of two years I grew comfortable programming arduinos to integrate with sensors and actuators as well as post and request data from the internet.",
+        date: "2015-Present",
+        src: source3
     },
     panel5:{
         title: "BACK TO SCHOOL",
-        text: "I was not always a tech person, In fact I daul majored in international politics & Japanese Language and Literature. However, by my senior year, I decided against pursuing a political career and began searching for other possible career paths. During my final semester of college I took an introduction to Python course and found that I am far more interested in technology than politics.",
-        date: "May-2014",
-        src: source5
+        text: "By mid 2017 I earned my Associates Degree in Engineering Technology at Hillsborough Community College. However, after a few unfruitful months of applying for jobs with my new AS degree, I decided to go all the way for a second BA and enrolled in the computer engineering undergraduate program at Florida Polytechnic University. Florida Poly is an excellent school with great opportunities, but I quickly realized another 4 years of university was not the best route forward for me.",
+        date: "2016 - 2017",
+        src: source4
     },
     panel6:{
         title: "UCF CODING BOOTCAMP",
-        text: "I was not always a tech person, In fact I daul majored in international politics & Japanese Language and Literature. However, by my senior year, I decided against pursuing a political career and began searching for other possible career paths. During my final semester of college I took an introduction to Python course and found that I am far more interested in technology than politics.",
-        date: "May-2014",
-        src: source3
+        text: "Unwilling to devote another 4 years to seeking a second BA before pursuing a new job, I sought another path to certification and enrolled in the Trilogy UCF Coding Bootcamp in August of 2018. The camp was very intensive and emphasized collaboration and coding real world relevant projects. Since graduating the camp my skills, knowledge, and confidence as a developer have skyrocketed, yet I know my journey as a young junior developer has only just begun.",
+        date: "August - September 2018",
+        src: source5
+    },
+    panel7:{
+        title:"CURRENTLY..",
+        text: "pending...",
+        date: "Present",
+        src: ""
     }
 }
 
