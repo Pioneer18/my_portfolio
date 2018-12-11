ae7c646009377f2ffe4cfc5c210f9f5b7b495de5
sellsj14 <sellsj14@gmail.com>
2018-12-11 15:23:45 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my_portfolio/precache-manifest.28b574f24eaa941a98c77050dcb48699.js"
+  "/my_portfolio/precache-manifest.22c0acd9f0ca415350b5bb99428c753f.js"
 );
 
 workbox.clientsClaim();
