8191feaf64deb6becf7dda3ca8e3d4c0524e9b00
sellsj14 <sellsj14@gmail.com>
2018-12-13 01:29:26 -0500

    changed parallax to regular div with background image, not attachment fixed effect; it seemed to be causing lag with the animation right below it

--- jonathansells/src/components/Gallery/Project/Project.js
+++ jonathansells/src/components/Gallery/Project/Project.js
@@ -20,7 +20,7 @@ export default Project;
 import React from 'react';
 import { Animation } from 'mdbreact';
 const Project = props => (
-    <Animation reveal type="fadeInUp">
+    <Animation reveal type="fadeIn" duration="2s">
         <div style={{background:`url(${props.url})`, backgroundPosition:`center`, backgroundSize:`cover`, backgroundRepeat: `no-repeat`}} className="project">
             <div className="overlay">
                 <div className="text"><a className="project-link" href={props.href}>{props.link}</a></div>
